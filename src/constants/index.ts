71fb15097f5225c839732eac8c303992a9e0e99b
guilhermeabell <gpeuvis@gmail.com>
2023-05-14 01:36:17 -0300

    refactor: replacing enums to object constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
-export enum KEY_MAP {
-  ENTER = 13,
-  UP = 40,
-  DOWN = 38,
-}
+export const KEY_MAP = {
+  ENTER: { code: 'Enter', key: 'Enter' },
+  UP: { code: 'ArrowUp', key: 'ArrowUp' },
+  DOWN: { code: 'ArrowDown', key: 'ArrowDown' },
+};
 
 export const MAX_RESULTS_HEIGHT = 500;
